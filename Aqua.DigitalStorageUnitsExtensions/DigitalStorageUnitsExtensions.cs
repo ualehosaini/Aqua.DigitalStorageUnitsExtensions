64bdf6bb3b9378122e83dbc47292d2ef46e64dc3
ualehosaini2019 <ualehosaini2019@hotmail.com>
2019-12-03 03:31:19 +0000

    ByteToKiloByte has been added

---
+++ Aqua.DigitalStorageUnitsExtensions/DigitalStorageUnitsExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Aqua.DigitalStorageUnitsExtensions
+{
+    public static class DigitalStorageUnitsExtensions
+    {
+        private const decimal KiloByte = 1024;
+
+        /// <summary>
+        /// Converts Byte to KiloByte
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static decimal ByteToKiloByte(this decimal input)
+        {
+            return input / KiloByte;
+        }
+
+    }
+}
