18c95a905f7fc817befc9bc3b857fc2e89f7894d
ualehosaini2019 <ualehosaini2019@hotmail.com>
2019-12-04 02:11:53 +0000

    ByteToMegaByte has been added

--- Aqua.DigitalStorageUnitsExtensions/DigitalStorageUnitsExtensions.cs
+++ Aqua.DigitalStorageUnitsExtensions/DigitalStorageUnitsExtensions.cs
@@ -3,6 +3,7 @@
     public static class DigitalStorageUnitsExtensions
     {
         private const decimal KiloByte = 1024;
+        private const decimal Megabyte = 1048576;
 
         /// <summary>
         /// Converts Byte to KiloByte
@@ -22,6 +23,16 @@
         public static decimal KiloByteToByte(this decimal input)
         {
             return input * KiloByte;
+        }
+
+        /// <summary>
+        /// Converts Byte to MegaByte
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static decimal ByteToMegaByte(this decimal input)
+        {
+            return input / Megabyte;
         }
 
     }
